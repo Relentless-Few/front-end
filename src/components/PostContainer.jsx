4b4e5ee6bbba3f5386b6e22b46fcd7ee95f05a32
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-07 23:09:33 -0400

    Basic layout of posts and comments

---
+++ src/components/PostContainer.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_USERS } from "../queries/userQueries";
+import CommentContainer from "./CommentContainer";
+
+const PostContainer = props => {
+  const { loading, error, data } = useQuery(QUERY_USERS);
+  // Will need another query for posts
+  // will most likely need another query for retreiving post's comments
+
+  // const onSubmit = () => {
+  //     if (!loading) {
+  //       console.log(data);
+  //   props.userLogon(props);
+  //     }
+  // };
+
+  useEffect(() => {
+    if (!loading) {
+      console.log(data)
+    }
+  })
+
+  return (
+    <div style={{ width: "450px", padding: "10px", marginTop: "20px", borderRadius: "10px", backgroundColor: "lightgray"}}>
+      <h3>Tiffany Simionescu</h3>
+      <p>{!loading ? data : ''}</p>
+      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit 
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+        occaecat cupidatat non proident, sunt in culpa qui officia 
+        deserunt mollit anim id est laborum.</h4>
+      <h3>Comments:</h3>
+      <CommentContainer />
+    </div>
+  )
+};
+
+export default PostContainer;
