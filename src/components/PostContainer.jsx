2c7410d28ca19c740a3334c316d7f4aaae683e9f
Tiffany Simionescu <42684183+tiffany-simionescu@users.noreply.github.com>
2020-08-08 14:01:07 -0400

    Updating Forms (#4)
    * Basic layout of posts and comments
    
    * Buttons create & join teams & orgs, initial forms
    
    * Added post & comment forms. Updated file structure
    
    * Updated PostForm

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_USERS } from "../queries/userQueries";
 import CommentContainer from "./CommentContainer";
+import Button from "@material-ui/core/Button";
+import { sharedStyles } from "../materialui/styles/styles";
 
 const PostContainer = props => {
   const { loading, error, data } = useQuery(QUERY_USERS);
+
+  const classes = sharedStyles();
   // Will need another query for posts
   // will most likely need another query for retreiving post's comments
 
@@ -22,20 +26,29 @@ const PostContainer = props => {
   }, [data])
 
   return (
-    <div style={{ width: "450px", padding: "10px", marginTop: "20px", borderRadius: "10px", backgroundColor: "lightgray"}}>
-      <h3>Tiffany Simionescu</h3>
-      {/* <p>{!loading ? data : ''}</p> */}
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit 
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-        occaecat cupidatat non proident, sunt in culpa qui officia 
-        deserunt mollit anim id est laborum.</h4>
-      <h3>Comments:</h3>
-      <div>
-      <CommentContainer />
+    <div>
+      <Button
+        variant="contained"
+        onClick={() => props.history.push("/createpost")}
+        className={classes.standardButton}
+      >
+        Create Post
+      </Button>
+      <div style={{ width: "450px", padding: "10px", marginTop: "20px", borderRadius: "10px", backgroundColor: "lightgray"}}>
+        <h3>Tiffany Simionescu</h3>
+        {/* <p>{!loading ? data : ''}</p> */}
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit 
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+          occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.</h4>
+        <h3>Comments:</h3>
+        <div>
+        <CommentContainer />
+        </div>
       </div>
     </div>
   )
